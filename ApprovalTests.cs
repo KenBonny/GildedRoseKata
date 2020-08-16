bfd697317fb20c7ae5148d9d040804c88e9dc02c
Ken Bonny <bonny.ken+github@gmail.com>
2020-08-16 21:19:15 +0200

    set up gilded rose kata

---
+++ ApprovalTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Xunit;
+
+namespace KenBonny.GildedRoseKata
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class ApprovalTests
+    {
+        [Fact]
+        public void ThirtyDays()
+        {
+            StringBuilder fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Program.Main();
+            var output = fakeoutput.ToString();
+
+            Approvals.Verify(output);
+        }
+    }
+}
