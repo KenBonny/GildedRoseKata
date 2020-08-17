f855dfcffe94f658d67bdba3d7fe5805cab65224
Ken Bonny <bonny.ken+github@gmail.com>
2020-08-17 15:40:28 +0200

    add test to check for expired conjured items

--- GildedRoseTests.cs
+++ GildedRoseTests.cs
@@ -6,12 +6,21 @@ namespace KenBonny.GildedRoseKata
     public class GildedRoseTests
     {
         [Fact]
-        public void ConjuredItemsDegradeTwiceAsFast()
+        public void NonExpiredConjuredItemsDegradeTwiceAsFast()
         {
             IList<Item> items = new List<Item> { new Item { Name = "Conjured cake", SellIn = 5, Quality = 10 } };
             GildedRose app = new GildedRose(items);
             app.UpdateQuality();
             Assert.Equal(8, items[0].Quality);
+        }
+
+        [Fact]
+        public void ExpiredConjuredItemsDegradeTwiceAsFast()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Conjured cake", SellIn = -1, Quality = 10 } };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(6, items[0].Quality);
         }
     }
 }
