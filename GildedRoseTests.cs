7e2607a2f789eb31c539e901ddfbfc46e0cd955a
Ken Bonny <bonny.ken+github@gmail.com>
2020-08-17 10:58:27 +0200

    give more descriptive name to test so it is more clear what is being tested

--- GildedRoseTests.cs
+++ GildedRoseTests.cs
@@ -6,7 +6,7 @@ namespace KenBonny.GildedRoseKata
     public class GildedRoseTests
     {
         [Fact]
-        public void foo()
+        public void ConjuredItemsDegradeTwiceAsFast()
         {
             IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(items);
