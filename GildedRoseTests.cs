50f0b203977b8988a458627eb06b8d8f89b60f08
Ken Bonny <bonny.ken+github@gmail.com>
2020-08-17 10:55:42 +0200

    update names to be in line with conventions

--- GildedRoseTests.cs
+++ GildedRoseTests.cs
@@ -8,10 +8,10 @@ namespace KenBonny.GildedRoseKata
         [Fact]
         public void foo()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
+            IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal("fixme", items[0].Name);
         }
     }
 }
